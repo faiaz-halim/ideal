af2baa2d22642e092588fbc0e57aa70e639add10
Adekunle Ishola <kunle1104@gmail.com>
2018-12-09 20:17:25 +1100

    Update contact.service.ts

--- assets/app/contact/contact.service.ts
+++ assets/app/contact/contact.service.ts
@@ -10,12 +10,12 @@ import { Message } from './message.model';
 
 @Injectable()
 export class ContactService {
-  baseUrl: string = 'http://localhost:4568';
+  baseUrl: string = 'https://idealautomotive.com.au';
   headers = new Headers({'Content-Type': 'application/json'});
   constructor(private http: Http ) {}
 
   addMessage(message:Message){
-      const url = 'http://localhost:3000/messages';
+      const url = 'https://idealautomotive.com.au/messages';
       let body = JSON.stringify(message);
       return this.http.post(url, body, {headers: this.headers})
         .map(response => response.json());
